de61533968f273cb3d9aa1b4bf7ff36266bc5d01
Shashila Heshan <shahshila.heshan@thebetmakers.com>
2020-03-14 20:31:43 +0530

    added logger function and package

--- src/helpers/database/index.js
+++ src/helpers/database/index.js
@@ -1,15 +1,29 @@
 import mongoose from 'mongoose';
 import config from '../../config/config';
+import logger from '../logger'
 
 export default async function makeDb() {
+
 	mongoose.connect(config.databaseUrl, {
+
 		useNewUrlParser: true,
+
 		useUnifiedTopology: true
+
 	}).then(() => {
+
+		logger().info(`Connected to  ${config.databaseUrl}`);
+
 		console.log('Connected to %s', config.databaseUrl);
+
 		console.log('Backend is up and running... \n');
+
 	}).catch(err => {
+
+		logger().info(`App starting error:  ${err.message}`);
+
 		console.error('App starting error:', err.message);
+
 		process.exit(1);
 	});
 
@@ -18,7 +32,11 @@ export default async function makeDb() {
 	database.once('open', () => console.log('Connected to the database'));
 
 	// checks if connection with the database is successful
-	database.on('error', console.error.bind(console, 'MongoDB connection error'));
+	database.on('error', ()=>{
+		logger().info('Connection error');
+
+		console.log('Mongo db connection error')
+	});
 
 	return database;
 }
--- src/helpers/logger/index.js
+++ src/helpers/logger/index.js
@@ -1,1 +1,21 @@
-export default function log() {}
+import winston from 'winston'
+
+require('winston-daily-rotate-file');
+
+export default function log() {
+
+    let transport = new (winston.transports.DailyRotateFile)({
+        filename: 'logs/application-%DATE%.log',
+        datePattern: 'YYYY-MM-DD',
+        zippedArchive: true,
+        maxSize: '20m',
+        maxFiles: '14d'
+    });
+
+    return  winston.createLogger({
+        transports: [
+            transport
+        ]
+    });
+
+}
