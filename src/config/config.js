b63955ba92e0270e19ac660fd9a3bd5539b6a198
Aravinda Rathnayake <aravindacloud@gmail.com>
2020-03-16 16:13:36 +0530

    Add response dispatcher, structure register and login responses and add db name into config file

--- src/config/config.js
+++ src/config/config.js
@@ -2,7 +2,8 @@ import dotenv from 'dotenv';
 dotenv.config();
 
 let config = {
-	databaseUrl: process.env.MONGODB_URL || null,
+	databaseUrl: process.env.MONGODB_URL_LOCAL || null,
+	databaseName: process.env.DATABASE_NAME,
 	apiPort: process.env.WEB_PORT || 9090,
 	jwtSecret: process.env.JWT_SECRET_KEY,
 	saltRounds: process.env.BCRYPT_ROUNDS,
