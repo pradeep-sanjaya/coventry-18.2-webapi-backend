34fbc82161cf3ff46bbc22dc90879b0ddb3cc0e3
Pradeep <pradeeps@techprovint.com>
2020-03-19 08:10:47 +0530

    image upload for products and categories

--- src/index.js
+++ src/index.js
@@ -7,6 +7,7 @@ import config from './config/config';
 import initializeDB from './helpers/database';
 
 import authenticateJWT from './middlewares/auth';
+import upload from './middlewares/uploadMiddleware';
 
 import { errorResponse } from './helpers/response/response-dispatcher';
 
@@ -16,10 +17,17 @@ import productRouter from './routes/product';
 import apiDocsRouter from './routes/api-docs';
 import categoryRouter from './routes/category';
 import metaDataRouter from './routes/meta-data';
+import imageRouter from './routes/image';
+
+
 
 import HttpResponseType from './models/http-response-type';
+import path from 'path';
 
 const app = express();
+
+//console.log(path.join(__dirname, 'public'));
+app.use('/public', express.static(path.join(__dirname, 'public')));
 
 app.use(cors());
 app.use(bodyParser.json());
@@ -31,6 +39,8 @@ app.use('/api/v1/categories', authenticateJWT, categoryRouter);
 app.use('/api/v1/meta', authenticateJWT, metaDataRouter);
 app.use('/api/v1/mail', mailRouter);
 app.use('/api/v1/auth', authRouter);
+app.use('/api/v1/image', upload.single('image'), imageRouter);
+
 app.use('/api-docs', apiDocsRouter);
 
 app.all('*', (req, res) => {
