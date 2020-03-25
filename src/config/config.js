30867ff166c47ca7d45dd6ec1faad8b0e4e4fbcd
Pradeep <pradeeps@techprovint.com>
2020-03-25 11:00:25 +0530

    change db host url

--- src/config/config.js
+++ src/config/config.js
@@ -2,9 +2,9 @@ import dotenv from 'dotenv';
 dotenv.config();
 
 let config = {
-    databaseUrl: process.env.MONGODB_URL_LOCAL || null,
+    databaseUrl: process.env.MONGODB_URL_LOCAL || process.env.MONGODB_URL_PROD,
     databaseName: process.env.DATABASE_NAME,
-    apiPort: process.env.WEB_PORT || 9090,
+    apiPort: process.env.PORT || process.env.WEB_PORT,
     jwtSecret: process.env.JWT_SECRET_KEY,
     saltRounds: process.env.BCRYPT_ROUNDS,
     sendGridApiKey: process.env.SENDGRID_API_KEY,
