5cc994b4dcb0b40459eb12db6394c2aca6b4a5d2
Pradeep <pradeeps@techprovint.com>
2020-03-27 06:25:05 +0530

    static image serve update

--- src/index.js
+++ src/index.js
@@ -12,14 +12,14 @@ import upload from './middlewares/uploadMiddleware';
 import { errorResponse } from './helpers/response/response-dispatcher';
 
 import authRouter from './routes/auth';
-import mailRouter from './routes/mail';
 import productRouter from './routes/product';
 import apiDocsRouter from './routes/api-docs';
 import categoryRouter from './routes/category';
 import metaDataRouter from './routes/meta-data';
+
 import imageRouter from './routes/image';
-
-
+import cartRouter from './routes/cart';
+import orderRouter from './routes/order';
 
 import HttpResponseType from './models/http-response-type';
 import path from 'path';
@@ -36,15 +36,19 @@ initializeDB();
 
 app.use('/api/v1/products', authenticateJWT, productRouter);
 app.use('/api/v1/categories', authenticateJWT, categoryRouter);
+app.use('/api/v1/orders', authenticateJWT, orderRouter);
 app.use('/api/v1/meta', authenticateJWT, metaDataRouter);
-app.use('/api/v1/mail', mailRouter);
+app.use('/api/v1/cart', authenticateJWT, cartRouter);
 app.use('/api/v1/auth', authRouter);
 app.use('/api/v1/image', upload.single('image'), imageRouter);
 
 app.use('/api-docs', apiDocsRouter);
 
 app.all('*', (req, res) => {
-    return errorResponse(res, HttpResponseType.NOT_FOUND, 'Request URL not found');
+    return errorResponse(res, {
+        code: HttpResponseType.NOT_FOUND,
+        message: 'Request URL not found'
+    });
 });
 
 app.listen(config.apiPort, () => console.log(`Listening on port ${config.apiPort}`));
